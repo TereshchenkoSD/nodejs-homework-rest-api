3edd3ca638654a13b620ad5766d932f77dbebbea
TereshchenkoSD <serghh1988@gmail.com>
2021-09-29 16:56:10 +0300

    Удалил папку model. Создал модель Contact с помощью библиотеки Mongoose для валидации данных. Заменил методы работы с коллекцией контактов на методы модели Contact. В файле server.js прописал логику подключения к БД с коллекцией контактов. Создал отдельную Joi схему валидации поля favourite

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,16 +1,19 @@
 const { NotFound } = require("http-errors");
-const { contactSchema } = require("../schemas");
+const { contactJoiSchema } = require("../models/contact");
 const { sendSuccessRes } = require("../helpers");
-const contactsOperations = require("../model");
+const { Contact } = require("../models");
 
 const getAll = async (req, res) => {
-  const result = await contactsOperations.listContacts();
+  const result = await Contact.find({}, "_id name phone email favourite");
   sendSuccessRes(res, { result });
 };
 
 const getById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contactsOperations.getContactById(contactId);
+  const result = await Contact.findById(
+    contactId,
+    "_id name phone email favourite"
+  );
   if (!result) {
     throw new NotFound(`Contact with the id-${contactId} was not found`);
   }
@@ -18,20 +21,19 @@ const getById = async (req, res) => {
 };
 
 const add = async (req, res) => {
-  const result = await contactsOperations.addContact(req.body);
+  const result = await Contact.create(req.body);
   sendSuccessRes(res, { result }, 201);
 };
 
 const updateById = async (req, res) => {
-  const { error } = contactSchema.validate(req.body);
+  const { error } = contactJoiSchema.validate(req.body);
   if (error) {
     throw new BadRequest(error.message);
   }
   const { contactId } = req.params;
-  const result = await contactsOperations.updateContactById(
-    contactId,
-    req.body
-  );
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
   if (!result) {
     throw new NotFound(`Contact with the id-${contactId} was not found`);
   }
@@ -40,11 +42,38 @@ const updateById = async (req, res) => {
 
 const removeById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contactsOperations.removeContact(contactId);
+  const result = await Contact.findByIdAndDelete(contactId);
   if (!result) {
     throw new NotFound(`Contact with the id-${contactId} was not found`);
   }
   sendSuccessRes(res, { message: "Success delete" });
 };
 
-module.exports = { getAll, getById, add, updateById, removeById };
+const updateFavourite = async (req, res) => {
+  const { error } = contactJoiSchema.validate(req.body);
+  if (error) {
+    throw new BadRequest(error.message);
+  }
+  const { contactId } = req.params;
+  const { favourite } = req.body;
+  const result = await Contact.findByIdAndUpdate(
+    contactId,
+    { favourite },
+    {
+      new: true,
+    }
+  );
+  if (!result) {
+    throw new NotFound(`Contact with the id-${contactId} was not found`);
+  }
+  sendSuccessRes(res, { result });
+};
+
+module.exports = {
+  getAll,
+  getById,
+  add,
+  updateById,
+  removeById,
+  updateFavourite,
+};

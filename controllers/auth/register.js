759a8476dd37ce178e62d456a76e6e8e8a3c2a27
TereshchenkoSD <serghh1988@gmail.com>
2021-10-20 14:26:47 +0300

    Installed nanoid package. Added properties verify and verifyToken to the User model. Created controllers verify and reverify. Added check for the property verify to login controller. Imported nanoid and sendEmail functions to the register controller. Used nanoid for creation of verifyToken property value. Added this property to the newUser object. Added two routes for verification and reverification processes.

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,6 +1,9 @@
 const { User } = require("../../models");
 const { Conflict } = require("http-errors");
 const gravatar = require("gravatar");
+const { nanoid } = require("nanoid");
+
+const { sendEmail } = require("../../helpers");
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -10,16 +13,29 @@ const register = async (req, res) => {
   }
 
   const newAvatar = gravatar.url(email, { s: "200" }, true);
-
-  const newUser = new User({ email });
+  const verifyToken = nanoid();
+  const newUser = new User({ email, verifyToken });
   newUser.setPassword(password);
   newUser.setAvatar(newAvatar);
 
   await newUser.save();
+
+  const data = {
+    to: email,
+    subject: "Confirmation of signup on the web-site",
+    html: `
+    <a href="http://localhost:3000/api/auth/verify/${verifyToken}" target="_blank">Confirm the email</a>
+    `,
+  };
+
+  await sendEmail(data);
   res.status(201).json({
     status: "success",
     code: 201,
     message: "Success register",
+    data: {
+      verifyToken,
+    },
   });
 };
 
